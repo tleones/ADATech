85e629863426587a73bd92aa68331d06d3d5c250
Tomás Leones <tomasleones@gmail.com>
2024-01-22 15:04:06 -0300

    Adicionando estrutura da Aula2

---
+++ Node.js com Express (Intermediário)/Aula2/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Recurso Obtido' });
+  });
+  
+app.post('/', (req, res) => {
+    res.json({ message: 'Recurso Criado' });
+  });
+  
+app.put('/', (req, res) => {
+    res.json({ message: 'Recurso Atualizado' });
+  });
+  
+app.delete('/', (req, res) => {
+    res.json({ message: 'Recurso Excluído' });
+  });
+  
